960f23e7721b9a164026c425de1f044df33a9c6c
Jeppe Mark Møller <jeppemmoeller@gmail.com>
2018-04-12 17:20:32 +0200

    working pagerank -- add more notes

--- pagerank/pagerank.py
+++ pagerank/pagerank.py
@@ -9,24 +9,41 @@ import networkx as nx
 import sys
 import time as tm
 
+# =============================================================================
+# Pagerank using dictonaries
+# =============================================================================
+
 time0=tm.time()
 with open(sys.argv[1], "rb") as infile:
     next(infile)
     G=nx.read_adjlist(infile, create_using=nx.DiGraph(),nodetype=int)
-    GS = nx.stochastic_graph(G,copy=True)
-    no_nodes = GS.number_of_nodes()
-    m = 0.15#dampening factor
-    nodes = G.nodes()
-    edges = G.edges()
-    degree_dict = {}#keeps in_degree of all nodes
-    dangling_dict = {}
-    page_rank = {}#dict to return rank of nodes
-    visit_dict = {}#dict to keep track of nr. of visits for each node
-    for node in nodes:#Enter nodes and their in_degrees to degree_dict (if they aren't dangling)
-        if G.in_degree(node) != 0:
-            degree_dict[node]=G.in_degree(node)/no_nodes
-        else:
-            dangling_dict[node]=m
-    print(degree_dict)
-#    print(tm.time()-time0)
-#    page_list = [[0]*no_nodes]*no_nodes
+    graph_out = nx.stochastic_graph(G,copy=True)#graph where out-edges of each node has a sum of 1 
+#    print(type(graph_out))
+    no_nodes = graph_out.number_of_nodes()#nodes in graph
+    damp = 0.15#dampening factor
+    rank_dict = dict.fromkeys(graph_out, 1.0 / no_nodes)#contains nodes and their rank. Initially importance is uniformally distributed among nodes 
+    p = dict.fromkeys(graph_out, 1.0 / no_nodes)
+    dangling_weight_dict = p
+    dangling_nodes = [node for node in graph_out if graph_out.out_degree(node,weight="weight") == 0.0]#contains nodes with no initial weight
+    
+    check = True
+    while check == True:
+        rank_dict_copy = rank_dict #
+        rank_dict = dict.fromkeys(rank_dict_copy.keys(), 0)
+        dangle_sum = damp * sum(rank_dict_copy[node] for node in dangling_nodes)
+        for node in rank_dict:
+            for n in graph_out[node]:
+                rank_dict[n] += damp * graph_out[node][n]["weight"] * rank_dict_copy[node]
+            rank_dict[node] += dangle_sum * dangling_weight_dict[node] + p[node] * (1.0 - damp)#x_{k+1} = (1 − m)Axk + (1 − m)Dxk + mSxk
+        if rank_dict_copy == rank_dict: #if there is no change after computations returns rank_dict
+            check = False#stops loop
+            print("\n")
+            print("The ranking of each node is as follows:","\n")
+            for k,v in rank_dict.items():
+                print("node {}, rank: {}".format(k,v),"\n")
+            print("==================================================","\n")
+    print("networkx's pagerank gives: ","\n")
+    page_rank=nx.pagerank(G,alpha=0.15)
+    for k,v in page_rank.items():
+        print("node {}, rank: {}".format(k,v),"\n")
+    print("it took {} seconds".format(tm.time()-time0))
