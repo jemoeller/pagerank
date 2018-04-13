8bdbf8889fd488bda4c14c0a7089a5c0489061f5
Jeppe Mark Møller <jeppemmoeller@gmail.com>
2018-04-13 22:54:08 +0200

    done did it

--- pagerank/pagerank.py
+++ pagerank/pagerank.py
@@ -5,30 +5,25 @@ Created on Wed Apr  4 13:18:29 2018
 @author: jeppe
 """
 import networkx as nx
-#import math
 import sys
 import time as tm
+from collections import Counter
 
 # =============================================================================
 # Pagerank using dictonaries
 # =============================================================================
-
-time0=tm.time()
-with open(sys.argv[1], "rb") as infile:
-    next(infile)
-    G=nx.read_adjlist(infile, create_using=nx.DiGraph(),nodetype=int)
-    graph_out = nx.stochastic_graph(G,copy=True)#graph where out-edges of each node has a sum of 1 
-#    print(type(graph_out))
+def page_rank(graph):
+    graph_out = nx.stochastic_graph(graph,copy=True)#graph where out-edges of each node has a sum of 1 
     no_nodes = graph_out.number_of_nodes()#nodes in graph
     damp = 0.15#dampening factor
     rank_dict = dict.fromkeys(graph_out, 1.0 / no_nodes)#contains nodes and their rank. Initially importance is uniformally distributed among nodes 
-    p = dict.fromkeys(graph_out, 1.0 / no_nodes)
-    dangling_weight_dict = p
+    p = dict.fromkeys(graph_out, 1.0 / no_nodes)#
+    dangling_weight_dict = p#
     dangling_nodes = [node for node in graph_out if graph_out.out_degree(node,weight="weight") == 0.0]#contains nodes with no initial weight
-    
     check = True
+#    print(dangling_nodes)
     while check == True:
-        rank_dict_copy = rank_dict #
+        rank_dict_copy = rank_dict
         rank_dict = dict.fromkeys(rank_dict_copy.keys(), 0)
         dangle_sum = damp * sum(rank_dict_copy[node] for node in dangling_nodes)
         for node in rank_dict:
@@ -37,13 +32,26 @@ with open(sys.argv[1], "rb") as infile:
             rank_dict[node] += dangle_sum * dangling_weight_dict[node] + p[node] * (1.0 - damp)#x_{k+1} = (1 − m)Axk + (1 − m)Dxk + mSxk
         if rank_dict_copy == rank_dict: #if there is no change after computations returns rank_dict
             check = False#stops loop
-            print("\n")
-            print("The ranking of each node is as follows:","\n")
-            for k,v in rank_dict.items():
-                print("node {}, rank: {}".format(k,v),"\n")
-            print("==================================================","\n")
-    print("networkx's pagerank gives: ","\n")
-    page_rank=nx.pagerank(G,alpha=0.15)
-    for k,v in page_rank.items():
-        print("node {}, rank: {}".format(k,v),"\n")
-    print("it took {} seconds".format(tm.time()-time0))
+            return rank_dict
+
+
+if __name__ == "__main__":
+    time0=tm.time()
+    with open(sys.argv[1], "rb") as infile:
+        next(infile)
+        G=nx.read_adjlist(infile, create_using=nx.DiGraph(),nodetype=int)#directed graph
+        rank = page_rank(G)
+        count = Counter(rank)
+        num = 1
+        print("\n")
+        print("The ranking of each node is as follows:","\n")
+        for k,v in count.most_common(10):
+            if num < 10:
+                print("({}) |node nr.{}| rank: {}".format(num,k,v),"\n")
+            num += 1
+#        print("==================================================","\n")
+#        for k,v in rank.items():
+#            print("node {}, rank: {}".format(k,v),"\n")
+#         print("==================================================","\n")
+    print("Calculations took {} seconds.".format(tm.time()-time0))
+        
---
+++ untitled0.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 12 17:49:03 2018
+
+@author: jeppe
+"""
+
+ddict = {}
+if ddict["hej"]:
+    print("whaat")
